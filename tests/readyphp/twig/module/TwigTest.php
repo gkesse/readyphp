62a3abb231f347e42e407f616e35a7117ce48162
Gerard KESSE <tiakagerard@hotmail.com>
2026-09-22 05:43:40 +0200

    feat(pre-commit): configure la validation stricte des types php lors du pre-commit

--- tests/readyphp/twig/module/TwigTest.php
+++ tests/readyphp/twig/module/TwigTest.php
@@ -5,12 +5,13 @@ declare(strict_types=1);
 namespace readyphp\twig\module;
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Assert;
 
 // cree un mock du controleur twig pour les tests
 class TwigMock extends \readyphp\twig\controller\Twig
 {
     // recupere le repertoire des templates twig
-    public function getTemplateDir()
+    public function getTemplateDir(): string
     {
         return sys_get_temp_dir() . '/readytests';
     }
@@ -21,7 +22,11 @@ class TwigMock extends \readyphp\twig\controller\Twig
         return 'template.twig';
     }
 
-    // recupere les donnees du template twig
+    /**
+     * Recupere les donnees du template twig
+     *
+     * @return array<string, string>
+     */
     public function getTemplateData(): array
     {
         return ['name' => 'MON_NOM'];
@@ -30,74 +35,57 @@ class TwigMock extends \readyphp\twig\controller\Twig
 
 class TwigTest extends TestCase
 {
-    private $m_template_dir;
-    private $m_template_file;
-    private $m_template_path;
+    private string $m_template_dir;
+    private string $m_template_file;
+    private string $m_template_path;
 
     // initialise le test
-    private function initialiser_test()
+    private function initialiser_test(): void
     {
-        // recupere le repertoire temporaire
-        $tmp_dir = sys_get_temp_dir();
-        // definit le repertoire temporaire pour les tests
+        $tmp_dir               = sys_get_temp_dir();
         $this->m_template_dir  = "$tmp_dir/readytests";
-        // definit le fichier du template twig pour les tests
         $this->m_template_file = 'template.twig';
-        // definit le chemin complet du fichier du template twig pour les tests
         $this->m_template_path = "$this->m_template_dir/$this->m_template_file";
 
-        // cree le repertoire temporaire pour les tests
         if (!is_dir($this->m_template_dir)) {
             mkdir($this->m_template_dir, 0777, true);
         }
 
-        // cree le template twig pour les tests
         $this->initialiser_template_twig();
     }
 
     // initialise le template twig
-    private function initialiser_template_twig()
+    private function initialiser_template_twig(): void
     {
-        // cree le contenu du template twig
         $template_content = "Bonjour : {{ name }} !\n";
-        // cree le fichier du template twig
         file_put_contents($this->m_template_path, $template_content);
     }
 
     // nettoie le test
-    private function nettoyer_test()
+    private function nettoyer_test(): void
     {
-        // supprime le fichier du template twig
         if (file_exists($this->m_template_path)) {
             unlink($this->m_template_path);
         }
 
-        // supprime le repertoire temporaire pour les tests
         if (is_dir($this->m_template_dir)) {
             rmdir($this->m_template_dir);
         }
     }
 
     // teste le rendu du module twig
-    public function test_rendu_module_twig()
+    public function test_rendu_module_twig(): void
     {
-        // definit le resultat attendu
         $DEF_OUTPUT = "Bonjour : MON_NOM !\n";
 
-        // initialise le test
         $this->initialiser_test();
 
-        // cree le controller et le module twig
         $controller = new TwigMock();
         $module     = new Twig($controller);
 
-        // execute le module twig
         $output = $module->render();
 
-        // teste le rendu du module twig
-        $this->assertEquals($DEF_OUTPUT, $output);
-
-        // nettoie le test
+        Assert::assertEquals($DEF_OUTPUT, $output);
         $this->nettoyer_test();
     }
 }
--- tests/readytests/twig/TwigTest.php
+++ tests/readytests/twig/TwigTest.php
@@ -5,79 +5,63 @@ declare(strict_types=1);
 namespace readytests\twig;
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Assert;
 
 require __DIR__ . '/../../../vendor/autoload.php';
 
 class TwigTest extends TestCase
 {
-    private $m_template_dir;
-    private $m_template_file;
-    private $m_template_path;
+    private string $m_template_dir;
+    private string $m_template_file;
+    private string $m_template_path;
 
     // initialise le test
-    private function initialiser_test()
+    private function initialiser_test(): void
     {
-        // recupere le repertoire temporaire
-        $tmp_dir = sys_get_temp_dir();
-        // definit le repertoire temporaire pour les tests
+        $tmp_dir               = sys_get_temp_dir();
         $this->m_template_dir  = "$tmp_dir/readytests";
-        // definit le fichier du template twig pour les tests
         $this->m_template_file = 'template.twig';
-        // definit le chemin complet du fichier du template twig pour les tests
         $this->m_template_path = "$this->m_template_dir/$this->m_template_file";
 
-        // cree le repertoire temporaire pour les tests
         if (!is_dir($this->m_template_dir)) {
             mkdir($this->m_template_dir, 0777, true);
         }
 
-        // cree le template twig pour les tests
         $this->initialiser_template_twig();
     }
 
     // initialise le template twig
-    private function initialiser_template_twig()
+    private function initialiser_template_twig(): void
     {
-        // cree le contenu du template twig
         $template_content = "Bonjour : {{ name }} !\n";
-        // cree le fichier du template twig
         file_put_contents($this->m_template_path, $template_content);
     }
 
     // nettoie le test
-    private function nettoyer_test()
+    private function nettoyer_test(): void
     {
-        // supprime le fichier du template twig
         if (file_exists($this->m_template_path)) {
             unlink($this->m_template_path);
         }
 
-        // supprime le repertoire temporaire pour les tests
         if (is_dir($this->m_template_dir)) {
             rmdir($this->m_template_dir);
         }
     }
 
     // teste l'execution du process
-    public function test_execution_process()
+    public function test_execution_process(): void
     {
-        // definit le resultat attendu
         $DEF_OUTPUT = "Bonjour : MON_NOM !\n";
 
-        // initialise le test
         $this->initialiser_test();
 
-        // cree le loader et l'environnement Twig
         $loader = new \Twig\Loader\FilesystemLoader($this->m_template_dir);
         $twig   = new \Twig\Environment($loader);
 
-        // execute le rendu du template Twig
         $output = $twig->render($this->m_template_file, ['name' => 'MON_NOM']);
 
-        // teste l'execution du process
-        $this->assertEquals($DEF_OUTPUT, $output);
-
-        // nettoie le test
+        Assert::assertEquals($DEF_OUTPUT, $output);
         $this->nettoyer_test();
     }
 }
